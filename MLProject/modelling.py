4094ecea1d72dd8d7acd280f07cc87a632036cb3
david sebas s <144750420+david123410@users.noreply.github.com>
2025-12-16 23:20:56 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -9,10 +9,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-
 DATA_PATH = "gender_classification_preprocessed.csv"
 RUN_ID_PATH = os.path.join(os.path.dirname(__file__), "run_id.txt")
-
 
 def train():
     print("Memuat dataset preprocessed...")
@@ -22,7 +20,10 @@ def train():
     y = df["gender"]
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42, stratify=y
+        X, y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y
     )
 
     scaler = StandardScaler()
@@ -36,9 +37,13 @@ def train():
         preds = model.predict(X_test)
         acc = accuracy_score(y_test, preds)
 
+        # Log parameter
+        mlflow.log_param("model_type", "LogisticRegression")
+        mlflow.log_param("max_iter", 1000)
+
         # Log metric
         mlflow.log_metric("accuracy", acc)
-        
+
         mlflow.sklearn.log_model(
             sk_model=model,
             artifact_path="model"
@@ -49,7 +54,6 @@ def train():
             f.write(run.info.run_id)
 
         print(f"Training selesai. Run ID: {run.info.run_id}")
-
 
 if __name__ == "__main__":
     train()
