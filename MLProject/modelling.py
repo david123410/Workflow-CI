e45562cb80f089784287d967748d7431e0f7cb17
david sebas s <144750420+david123410@users.noreply.github.com>
2025-12-18 14:20:06 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,74 +1,50 @@
-import os
-import shutil
 import pandas as pd
 import mlflow
-from mlflow.sklearn import save_model
+import mlflow.sklearn
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+
 DATA_FILE = "gender_classification_preprocessed.csv"
 RUN_ID_FILE = "run_id.txt"
-TEMP_MODEL_DIR = "temp_model_dir"
 
+mlflow.autolog()
 
 def load_dataset(path):
-    print(" Membaca dataset...")
+    print("Membaca dataset...")
     return pd.read_csv(path)
-
 
 def prepare_data(df):
     X = df.drop("gender", axis=1)
     y = df["gender"]
 
     return train_test_split(
-        X,
-        y,
+        X, y,
         test_size=0.2,
         random_state=42,
         stratify=y
     )
 
-
 def train_and_log():
     df = load_dataset(DATA_FILE)
     X_train, X_test, y_train, y_test = prepare_data(df)
 
-    model = LogisticRegression(max_iter=1000)
-
-    # Bersihkan folder sementara
-    if os.path.exists(TEMP_MODEL_DIR):
-        shutil.rmtree(TEMP_MODEL_DIR)
-
     with mlflow.start_run() as run:
+        model = LogisticRegression(max_iter=1000)
         model.fit(X_train, y_train)
 
         preds = model.predict(X_test)
         acc = accuracy_score(y_test, preds)
 
-        # Log metric & param
-        mlflow.log_metric("accuracy", acc)
-        mlflow.log_param("model_type", "LogisticRegression")
-        mlflow.log_param("max_iter", 1000)
+        # Optional (boleh ada)
+        mlflow.log_metric("accuracy_manual", acc)
 
-        print(f"Akurasi model: {acc}")
-
-        # Simpan model ke folder sementara
-        save_model(model, TEMP_MODEL_DIR)
-
-        # Upload sebagai artifact "model"
-        mlflow.log_artifacts(TEMP_MODEL_DIR, artifact_path="model")
-
-        # Simpan run_id
+        # Simpan run_id untuk CI / Docker
         with open(RUN_ID_FILE, "w") as f:
             f.write(run.info.run_id)
 
-        print(f"🆔 Run ID disimpan: {run.info.run_id}")
-
-    # Bersihkan folder lokal
-    shutil.rmtree(TEMP_MODEL_DIR)
-    print("Folder sementara dibersihkan")
-
+        print(f"Run ID disimpan: {run.info.run_id}")
 
 if __name__ == "__main__":
     train_and_log()
